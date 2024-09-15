d8f7b2b50a6e850d8985e083e0f7b265ec2b68a8
0cirilolucas <lucas.cirilogomes@gmail.com>
2024-09-15 17:32:52 -0300

    listarmetas

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
-const { select, input } = require ('@inquirer/prompts') // buscando na pasta prompts as funções dentro de {}
+const { select, input, checkbox } = require ('@inquirer/prompts') // buscando na pasta prompts as funções dentro de {}
+
+let meta = {
+    value: 'Tomar 3L de água por dia',
+    checked: false,
+}
+
+let metas = [ meta ]
 
 const cadastrarMeta = async () => {
     const meta = await input ({ message: "Digite a meta: "})
 
-    if(meta.length == 0){ //meta.length == 0 significa que a leitura da função meta mostrou que não tem nenhum texto ou está vazia
+    if(meta.length == 0){ //meta.length == 0 significa que a leitura da constante 'meta' mostrou que não tem nenhum texto ou está vazia
         console.log('A meta não pode ser vazia!')
         return // aqui encerra a condição
         // return cadastrarMeta() -- Isso poderia ser usado para que o usuário ficasse preso até digitar a meta e passar para próxima etapa
 
     }
+
+    metas.push( //push é uma função! nesse caso vai colocar aqui dentro os objetos as metas criadas na "let metas"
+        {value: meta, checked: false }
+    )
+
 }
 
-const start= async () =>{
+const listarMetas = async () => {
+    const respostas = await checkbox ({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e Enter para finalizar essa etapa",
+        choices: [...metas],// aqui estamos fazendo uma cópia da let metas na linha 3, copiando todas as informações que entrarem em metas
+        instructions: false,
+    })
+
+    if(respostas.length ==0){
+        console.log("Nenhuma meta selecionada!")
+        return
+
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    //forEach significa para cada resposta, ele vai tomar uma ação
+    respostas.forEach((resposta) => {
+        // metas.find a função find vai procurar em meta 
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+        
+        meta.checked = true
+    })
+
+    console.log('Meta(s) concluída(s)')
+}
+
+const start = async () =>{
     
     while(true){
         //let opcao = 'cadastrar' //essa let opção está definindo para onde ele vai. Se eu colocar 'sair' ele vai para o case de sair e encerra o loop
@@ -42,6 +84,7 @@ const start= async () =>{
                     console.log("Vamos cadastrar")
                     break
                 case "listar":
+                    await listarMetas() 
                     console.log("Vamos listar")
                     break
                 case "sair":
