2f7b175ee269860b25096ecf2a589ccbd583a346
0cirilolucas <lucas.cirilogomes@gmail.com>
2024-10-08 11:21:06 -0300

    adciona metas abertas

--- index.js
+++ index.js
@@ -66,8 +66,24 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas Realizadas",
+        message: "Metas Realizadas "+ realizadas.length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () =>{
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+    })
+
+    if (abertas.length == 0){
+        console.log("Não existem metas abertas!")
+        return
+    }
+
+    await select({
+        message: "Metas Abertas " + abertas.length,//aqui ele mostra a quantidade de metas abertas com o "abertas.length"
+        choices: [...abertas]
     })
 }
 
@@ -93,6 +109,10 @@ const start = async () =>{
                     value: "realizadas"
                 },
                 {
+                    name: "Metas Abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -111,6 +131,9 @@ const start = async () =>{
                     break
                 case "realizadas":
                     await metasRealizadas()
+                    break
+                case "abertas":
+                    await metasAbertas()
                     break
                 case "sair":
                     console.log("Até a próxima!")
